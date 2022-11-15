1a800bb9f980af4681275216a7294a803d80c319
AlexChehov <0650343005id@gmail.com>
2022-11-15 15:46:00 +0200

    exam1

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ burger.addEventListener('click', (e) => {
     menuList.classList.toggle('list-active')
 })
 
+$('.team_team_team').slick({
+    arrows:false,
+    dots:true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+});
+    
